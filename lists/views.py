94f5bc696141eb95a86559230bb630a3291c0898
Iman van Assum <iman@vanassum.net>
2017-03-11 17:45:41 +0100

    looking for expected_error

--- lists/views.py
+++ lists/views.py
@@ -4,16 +4,14 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-    #items = Item.objects.all()
-    #return render(request, 'home.html', {'items': items})
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item.full_clean()
     return redirect('/lists/%d/' % (list_.id,))
 
 def add_item(request, list_id):
