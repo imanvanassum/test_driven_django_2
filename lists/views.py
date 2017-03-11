c7b4b065a81f54823b817fe549fc5fde38418e37
Iman van Assum <iman@vanassum.net>
2017-03-11 18:44:36 +0100

    Added model validation to new list view

--- lists/views.py
+++ lists/views.py
@@ -13,7 +13,9 @@ def new_list(request):
     item = Item.objects.create(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
+        item.save()
     except:
+        list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
