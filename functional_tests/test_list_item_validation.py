b221e32e24f8221b84fdee1e6c2c56a42e22f035
Iman van Assum <iman@vanassum.net>
2017-03-12 02:44:53 +0100

    Refactored list item validation FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit an empty
         # list item. She hits Enter on the empty input box
@@ -57,7 +60,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You already have this item on your list"
         ))
 
@@ -71,7 +74,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
 
         # She starts typing in the input box to clear the error
@@ -79,5 +82,5 @@ class ItemValidationTest(FunctionalTest):
 
         # She is pleased to see that the error message disappears
         self.wait_for(lambda: self.assertFalse(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
