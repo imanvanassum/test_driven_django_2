75992c64e6ddb9a0be6d3744d0b80c8eee66beee
Iman van Assum <iman@vanassum.net>
2017-03-12 01:33:48 +0100

    Funny business with the custom save method not working

--- lists/forms.py
+++ lists/forms.py
@@ -25,6 +25,9 @@ class ItemForm(forms.models.ModelForm):
         self.instance.list = for_list
         return super().save()
 
+
+
+
 class ExistingListItemForm(ItemForm):
 
     def __init__(self, for_list, *args, **kwargs):
@@ -37,3 +40,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
