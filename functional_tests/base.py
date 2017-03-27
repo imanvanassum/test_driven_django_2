7d8af0677ca8636653e215805817e7b45712dbf0
Iman van Assum <iman@vanassum.net>
2017-03-27 02:49:46 +0200

    Ansible is T-O-U-G-H

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -22,15 +22,9 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()            
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
